baa10a48aa51aa4a0e1a691daeabb2647a49c634
Richard Campos <richardsjcb@gmail.com>
2021-10-26 19:09:21 -0300

    IM-S# Se sube el file create con php, file_put_contents(archivo!, Texto)

--- prueba.php
+++ prueba.php
@@ -7,8 +7,10 @@
 /** Llamar a un archivo */
 
 
-$archivo = 'texto.txt';
-file_put_contents('texto.txt', $_GET['texto']); //-> Modifico donde va el texto por un Get, así se modifica desde una solicitud
+$archivo = $_GET['archivo'];
+file_put_contents($archivo, $_GET['texto']); //-> Modifico donde va el texto por un Get, así se modifica desde una solicitud
+/**La función file_put_contents () en PHP es una función incorporada que se usa para escribir una cadena en un archivo. 
+ * La función file_put_contents () busca el archivo en el que el usuario quiere escribir y si el archivo no existe, crea un nuevo archivo. */
 //put_contents muestra la variable en esté textaerea
 $texto = file_get_contents($archivo);
 //echo $texto;
@@ -20,9 +22,17 @@ $texto = file_get_contents($archivo);
 <h1> Editar Archivo de Texto </h1>
 
 <form action="">
-    <textarea name="texto" id="" cols="30" rows="10">
-<?= $texto ?>
-</textarea>
+    <label for="">Nombre del Archivo: </label>
+    <input type="text" name="archivo" value="<?= $archivo ?>"> <br>
+    <textarea name="texto"><?= $texto ?></textarea>
     <br />
     <input type="submit" value="Guardar">
 </form>
+
+<style>
+    textarea {
+        margin: 0px;
+        width: 676px;
+        height: 553px;
+    }
+</style>
